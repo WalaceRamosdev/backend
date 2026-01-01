28beec3ad36384f340ad0338f8a5c88d5180f08c
Walace Ramos <contatowalace.dev@gmail.com>
2026-01-01 06:34:49 -0300

    Corrigindo timeout do email

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.json()); // Permite receber JSON no corpo da requisição
 // Configuração do Nodemailer
 // O serviço 'gmail' é um atalho prático. Para outros, configure host e port.
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // false for 587, true for 465
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
